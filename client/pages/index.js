f4a7acc2a404855614d480b73e5f5e9013dc7a1b
justinjeong <justin.jeong5@gmail.com>
2021-01-18 21:01:22 +0900

    add(component): AppLayout.js
    AppLayout for page layout

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import AppLayout from '../components/AppLayout'
 
 function index() {
   return (
-    <div>
-      Hello, Next!
-    </div>
+    <AppLayout>
+      <div>Hello, Next!</div>
+    </AppLayout>
   )
 }
 
