1636f35f42787b2919aacdfd5134b42551a5d403
justinjeong <justin.jeong5@gmail.com>
2021-01-21 14:53:19 +0900

    add: responsive layout

--- client/components/AppLayout.js
+++ client/components/AppLayout.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Row, Col } from 'antd'
 import PropTypes from 'prop-types'
 import NavBar from './Header/NavBar'
 
@@ -6,7 +7,17 @@ function AppLayout({ children }) {
   return (
     <div>
       <NavBar />
-      {children}
+      <Row>
+        <Col xs={24} md={6}>
+          회원 정보
+        </Col>
+        <Col xs={24} md={12}>
+          {children}
+        </Col>
+        <Col xs={24} md={6}>
+          <a href='https://justinjeong5.github.io' target="_blank" rel="noreferrer noopener">made by JustinJeong5</a>
+        </Col>
+      </Row>
     </div>
   )
 }
