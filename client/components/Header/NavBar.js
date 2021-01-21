f88125ffd0bd9700026c7acbeb0afdfd4faf02d2
justinjeong <justin.jeong5@gmail.com>
2021-01-21 14:26:32 +0900

    refactor: menu using antd

--- client/components/Header/NavBar.js
+++ client/components/Header/NavBar.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import Link from 'next/link'
+import { Menu } from 'antd'
+import { TwitterOutlined, MailOutlined, AppstoreOutlined } from '@ant-design/icons'
 
 function NavBar() {
+
   return (
-    <div>
-      <Link href='/'><a>HOME</a></Link>
-      <Link href='/profile'><a>프로필</a></Link>
-      <Link href='/signup'><a>회원가입</a></Link>
-    </div>
+    <Menu mode="horizontal">
+      <Menu.Item key="homeMenu" icon={<TwitterOutlined />}>
+        <Link href='/'><a>트위터</a></Link>
+      </Menu.Item>
+      <Menu.Item key="profileMenu" icon={<MailOutlined />}>
+        <Link href='/profile'><a>프로필</a></Link>
+      </Menu.Item>
+      <Menu.Item key="signupMenu" icon={<AppstoreOutlined />}>
+        <Link href='/signup'><a>회원가입</a></Link>
+      </Menu.Item>
+    </Menu>
   )
 }
 
