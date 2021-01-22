6fdf23f90e81f6df1f76599682caf7ab102b2440
justinjeong <justin.jeong5@gmail.com>
2021-01-22 23:40:49 +0900

    add: image uploader using useRef

--- client/components/Post/PostForm.js
+++ client/components/Post/PostForm.js
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react'
+import React, { useCallback, useRef } from 'react'
 import { Form, Input, Button } from 'antd'
 import { useDispatch, useSelector } from 'react-redux'
 import { v4 as uuidv4 } from 'uuid'
@@ -7,6 +7,7 @@ import { ADD_POST } from '../../reducers/types';
 function PostForm() {
 
   const dispatch = useDispatch();
+  const imageUploadRef = useRef();
   const { imagePaths } = useSelector(state => state.post)
 
   const handleFinish = useCallback((values) => {
@@ -14,6 +15,10 @@ function PostForm() {
     dispatch({
       type: ADD_POST
     })
+  }, [])
+
+  const handleImageUpload = useCallback(() => {
+    imageUploadRef.current.click();
   }, [])
 
   return (
@@ -28,8 +33,8 @@ function PostForm() {
         placeholder='오늘의 이야기를 여러 사람들과 공유해 주세요'
       />
       <div>
-        <input type='file' multiple hidden />
-        <Button>이미지 업로드</Button>
+        <input type='file' multiple hidden ref={imageUploadRef} />
+        <Button onClick={handleImageUpload}>이미지 업로드</Button>
         <Button type='primary' style={{ float: 'right' }} htmlType='submit'>Tweet</Button>
       </div>
       <div>
