52476fc83cbf665e1935db53b457f5ddacdf20ce
justinjeong <justin.jeong5@gmail.com>
2021-01-25 16:22:19 +0900

    add: redirect when not-login user access

--- client/pages/profile.js
+++ client/pages/profile.js
@@ -1,14 +1,24 @@
-import React from 'react'
-import Head from 'next/head'
-import AppLayout from '../components/AppLayout'
-import NicknameEditForm from '../components/Profile/NicknameEditForm'
-import FollowList from '../components/Profile/FollowList'
-import faker from 'faker'
+import React, { useEffect } from 'react'
 import { useSelector } from 'react-redux'
+import Head from 'next/head'
+import Router from 'next/router'
+import AppLayout from '../components/AppLayout'
+import FollowList from '../components/Profile/FollowList'
+import NicknameEditForm from '../components/Profile/NicknameEditForm'
 
 function Profile() {
 
   const { currentUser } = useSelector(state => state.user)
+
+  useEffect(() => {
+    if (!currentUser.id) {
+      Router.push('/')
+    }
+  }, [currentUser])
+
+  if (!currentUser.id) {
+    return null;
+  }
   return (
     <>
       <Head>
