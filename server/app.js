fe753a88918ea1eb4578e6f49e5739ada95a1794
justinjeong <justin.jeong5@gmail.com>
2021-01-26 14:07:27 +0900

    add(server): express

--- server/app.js
+++ server/app.js
@@ -1,25 +1,10 @@
-const http = require('http');
-const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
-  if (req.method === 'GET') {
-    if (req.url === '/api/posts') {
-      return res.end('/api/posts, GET')
-    } else if (req.url === '/api/comment') {
-      return res.end('/api/comment, GET')
-    }
-  } else if (req.method === 'POST') {
-    if (req.url === '/api/post') {
-      return res.end('/api/post, POST')
-    }
-  }
-  else if (req.method === 'DELETE') {
-    if (req.url === '/api/post') {
-      return res.end('/api/post, DELETE')
-    }
-  }
-  res.end('Server Connected Successfully')
-});
+const express = require('express');
+const app = express();
 
-server.listen(3065, () => {
+app.get('/', (req, res) => {
+  res.send('Server Connected Successfully')
+})
+
+app.listen(3065, () => {
   console.log('Server is running')
 })
