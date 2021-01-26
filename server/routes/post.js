8fa11a4e33c0be5d83a97d2980235d7ea95a122c
justinjeong <justin.jeong5@gmail.com>
2021-01-26 23:43:05 +0900

    add(server): post, '/api/post/post'

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,8 +1,35 @@
 const express = require('express');
 
+const { Post, Image, User } = require('../models')
+const { loginRequired } = require('./middleware')
+
 const router = express.Router();
-router.post('/', (req, res) => {
-  res.send('/api/post/')
+
+router.post('/post', loginRequired, async (req, res, next) => {
+  try {
+    const post = await Post.create({
+      content: req.body.content,
+      UserId: req.user.id,
+    })
+    const fullPost = await Post.findOne({
+      where: { id: post.id },
+      include: [{
+        model: User,
+        attributes: {
+          exclude: ['password']
+        }
+      }, {
+        model: Image,
+      }, {
+        model: Post,
+        as: 'Retweet'
+      }]
+    })
+    return res.status(201).json({ message: '게시글이 정상적으로 등록되었습니다.', post: fullPost })
+  } catch (error) {
+    console.error(error)
+    next(error)
+  }
 })
 
 module.exports = router;
