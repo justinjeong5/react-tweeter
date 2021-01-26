1c1649a263d20bf95bb04cdb217e4712deedcb5e
justinjeong <justin.jeong5@gmail.com>
2021-01-26 14:23:48 +0900

    add(server): post router

--- server/app.js
+++ server/app.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 
+const post = require('./routes/post');
+
 app.get('/', (req, res) => {
   res.send('Server Connected Successfully')
 })
+
+app.use('/api/post', post)
 
 app.listen(3065, () => {
   console.log('Server is running')
