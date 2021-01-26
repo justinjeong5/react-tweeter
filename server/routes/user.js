5f3710a6652736a6d60c2b4ffa1e8756aac60bd9
justinjeong <justin.jeong5@gmail.com>
2021-01-26 17:38:51 +0900

    add(server): post '/api/user/register'

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,8 +1,29 @@
 const express = require('express');
+const bcrypt = require('bcryptjs')
+const { User } = require('../models')
 
 const router = express.Router();
-router.post('/', (req, res) => {
-  res.send('/api/user/')
+router.post('/register', async (req, res, next) => {
+  try {
+    const user = await User.findOne({
+      where: {
+        email: req.body.email
+      }
+    });
+    if (user) {
+      return res.status(403).json({ code: 'AlreadyExistUser', message: '이미 사용중인 이메일입니다.', });
+    }
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    await User.create({
+      email: req.body.email,
+      nickname: req.body.nickname,
+      password: hashedPassword,
+    });
+    res.status(201).json({ message: '회원가입이 정상적으로 완료되었습니다.' })
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
 })
 
 module.exports = router;
