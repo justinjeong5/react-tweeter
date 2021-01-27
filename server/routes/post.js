47ba5706fcac62405934b99001b682db22f7052e
justinjeong <justin.jeong5@gmail.com>
2021-01-27 11:40:11 +0900

    add(server): get, '/api/post/posts'
    using lastId & limit for infinite scroll

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,10 +1,46 @@
 const express = require('express');
+const { Op } = require('sequelize')
 
-const { Post, Image, User } = require('../models')
+const { Post, Comment, Image, User } = require('../models')
 const { loginRequired } = require('./middleware')
 
 const router = express.Router();
 
+router.get('/posts', loginRequired, async (req, res, next) => {
+  try {
+    const where = {};
+    const lastId = parseInt(req.query.lastId, 10);
+    console.log(lastId);
+    if (lastId) {
+      where.id = { [Op.lt]: lastId }
+    }
+    const posts = await Post.findAll({
+      where,
+      limit: 10,
+      order: [
+        ['createdAt', 'DESC'],
+        [Comment, 'createdAt', 'DESC']
+      ],
+      include: [{
+        model: User,
+        attributes: ['id', 'nickname'],
+      }, {
+        model: Image
+      }, {
+        model: Comment,
+        include: [{
+          model: User,
+          attributes: ['id', 'nickname'],
+        }]
+      }]
+    });
+    res.status(200).json({ message: '게시글 목록을 정상적으로 가져왔습니다.', posts })
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+
+})
 router.post('/post', loginRequired, async (req, res, next) => {
   try {
     const post = await Post.create({
