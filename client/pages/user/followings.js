2f6ee42de9148d190e9237d519eb6e9204610627
justinjeong <justin.jeong5@gmail.com>
2021-01-28 19:50:40 +0900

    fix(ssr): send cookie for ssr

--- client/pages/user/followings.js
+++ client/pages/user/followings.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { END } from 'redux-saga'
+import axios from 'axios'
 import wrapper from '../../store/configureStore'
 import MainPage from '../../components/LandingPage/MainPage'
 import { FOLLOWING, LOAD_CURRENT_USER_REQUEST, LOAD_POSTS_REQUEST } from '../../reducers/types'
@@ -12,6 +13,12 @@ function Followings() {
 }
 
 export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
+  axios.defaults.headers.Cookie = '';
+  const cookie = context.req ? context.req.headers.cookie : '';
+  if (cookie) {
+    axios.defaults.headers.Cookie = cookie;
+  }
+
   context.store.dispatch({
     type: LOAD_CURRENT_USER_REQUEST
   });
