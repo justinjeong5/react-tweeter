6ca513559de5ab3c8446d2e1ec5ab3d6ed903297
justinjeong <justin.jeong5@gmail.com>
2021-01-29 12:49:44 +0900

    fix: css ssr

--- client/pages/_document.js
+++ client/pages/_document.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import Document, { Html, Head, Main, NextScript } from 'next/document';
-import { ServerStyleSheet } from 'styled-components'
+import { ServerStyleSheet } from 'styled-components';
 
 export default class MyDocument extends Document {
-
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet();
     const originalRenderPage = ctx.renderPage;
 
     try {
       ctx.renderPage = () => originalRenderPage({
-        enhanceApp: (App) => (props) => sheet.collectStyles(<App {...props} />),
-      })
+        enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+      });
+
       const initialProps = await Document.getInitialProps(ctx);
       return {
         ...initialProps,
@@ -20,10 +20,8 @@ export default class MyDocument extends Document {
             {initialProps.styles}
             {sheet.getStyleElement()}
           </>
-        )
+        ),
       };
-    } catch (error) {
-      console.error(error)
     } finally {
       sheet.seal();
     }
