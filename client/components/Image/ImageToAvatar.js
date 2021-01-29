b684e7659f12f7a737e152f6a47dafcf211b3200
justinjeong <justin.jeong5@gmail.com>
2021-01-29 16:00:47 +0900

    refactor: image Url Parser

--- client/components/Image/ImageToAvatar.js
+++ client/components/Image/ImageToAvatar.js
@@ -2,23 +2,16 @@ import React, { useCallback } from 'react'
 import Router from 'next/router'
 import PropTypes from 'prop-types'
 import { Avatar } from 'antd'
+import { imageSrcParser } from '../utils/ImageSrcParser'
 
 function ImageToAvatar({ image, userId }) {
-
-  const srcParser = useCallback(() => {
-    if (image.src.includes('http://gravatar.com/avatar/')) {
-      return image.src
-    } else {
-      return `http://localhost:3065/${image.src}`
-    }
-  }, [image])
 
   const handleRouter = useCallback(() => {
     Router.push(`/user/${userId}`)
   }, [userId])
 
   return (
-    <span style={{ cursor: 'pointer' }} onClick={handleRouter}><Avatar src={srcParser()} /></span>
+    <span style={{ cursor: 'pointer' }} onClick={handleRouter}><Avatar src={imageSrcParser(image)} /></span>
   )
 }
 
---
+++ client/components/utils/ImageSrcParser.js
@@ -0,0 +1,15 @@
+import PropTypes from 'prop-types';
+
+export const imageSrcParser = (image) => {
+  if (image.src.includes('http://gravatar.com/avatar/')) {
+    return image.src
+  } else {
+    return `http://localhost:3065/${image.src}`
+  }
+}
+
+imageSrcParser.propTypes = {
+  image: PropTypes.shape({
+    src: PropTypes.string.isRequired
+  })
+}
