1645a90162d2490b42c612e08b8209364281dcea
justinjeong <justin.jeong5@gmail.com>
2021-01-29 19:53:37 +0900

    refactor: SSR for profile page

--- client/pages/profile.js
+++ client/pages/profile.js
@@ -1,32 +1,27 @@
 import React, { useEffect } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import { useSelector } from 'react-redux'
+import axios from 'axios'
 import Head from 'next/head'
 import Router from 'next/router'
-import { Skeleton } from 'antd'
+import { END } from 'redux-saga'
+import wrapper from '../store/configureStore'
 
 import AppLayout from '../components/AppLayout'
 import FollowList from '../components/Profile/FollowList'
 import NicknameEditForm from '../components/Profile/NicknameEditForm'
 import ImageEditForm from '../components/Profile/ImageEditForm'
 import UserProfile from '../components/User/UserProfile'
-import { GET_FOLLOW_REQUEST } from '../reducers/types'
+import { LOAD_CURRENT_USER_REQUEST, GET_FOLLOW_REQUEST } from '../reducers/types'
 
 function Profile() {
 
-  const dispatch = useDispatch();
-  const { currentUser, getFollowDone, getFollowLoading } = useSelector(state => state.user)
+  const { currentUser, editUserLoading, getFollowDone } = useSelector(state => state.user)
 
   useEffect(() => {
     if (!currentUser.id) {
       Router.push('/')
     }
   }, [currentUser])
-
-  useEffect(() => {
-    dispatch({
-      type: GET_FOLLOW_REQUEST
-    })
-  }, [])
 
   if (!currentUser.id) {
     return null;
@@ -36,12 +31,11 @@ function Profile() {
       <Head>
         <title>프로필</title>
       </Head>
-      <AppLayout UserProfile={<UserProfile User={currentUser} />}>
+      <AppLayout UserProfile={!editUserLoading && <UserProfile User={currentUser} />}>
         <div style={{ display: 'flex', justifyContent: 'center' }}>
           <div style={{ width: '100%', maxWidth: 600 }}>
             <NicknameEditForm />
             <ImageEditForm />
-            {getFollowLoading && <Skeleton />}
             {getFollowDone && <FollowList header='나를 팔로우 하는 사람' data={currentUser.Followers} />}
             {getFollowDone && <FollowList header='내가 팔로우 하는 사람' data={currentUser.Followings} />}
           </div>
@@ -50,5 +44,21 @@ function Profile() {
     </>
   )
 }
+
+export const getServerSideProps = wrapper.getServerSideProps(async (context) => {
+  axios.defaults.headers.Cookie = '';
+  const cookie = context.req ? context.req.headers.cookie : '';
+  if (cookie) {
+    axios.defaults.headers.Cookie = cookie;
+  }
+  context.store.dispatch({
+    type: LOAD_CURRENT_USER_REQUEST
+  });
+  context.store.dispatch({
+    type: GET_FOLLOW_REQUEST
+  });
+  context.store.dispatch(END);
+  await context.store.sagaTask.toPromise();
+});
 
 export default Profile
